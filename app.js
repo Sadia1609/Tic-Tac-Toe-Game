1372cdb0fcfe0e1b5762ac4d01f0c08b391a8c92
Sadia1609 <sadia.rahman160920@gmail.com>
2026-05-23 21:49:27 +0600

    checked winner pattern

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let resetBtn = document.querySelectorAll("#reset-btn");
 
 let turnO = true; //playerX, player0
 
-const winPtterns = [
+const winPatterns = [
   [0, 1, 2],
   [0, 3, 6],
   [0, 4, 8],
@@ -27,5 +27,25 @@ boxes.forEach((box) => {
       turnO = true;
     }
     box.disabled = true;
+
+    checkWinner();
   });
 });
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    // console.log(pattern[0],pattern[1],pattern[2]);
+    // console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
+
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if(pos1Val != "" && pos2Val != "" &pos3Val != ""){
+      if(pos1Val === pos2Val && pos2Val === pos3Val){
+        console.log("winner", pos1Val); //winning condition
+        
+      }
+    }
+  }
+};
